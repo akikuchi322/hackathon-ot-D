739a1fd26388203210bbea3d3a99d6a0309622df
hrito99513 <hrito99513@gmail.com>
2020-11-19 20:33:46 +0900

    追加要件1と3を実装

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -9,6 +9,36 @@ function publish() {
     //テキストを空にする
     $('#message').val('');
 
+    //空だけでの送信を認めない
+    if(message == ``){
+        alert(`メッセージが空(から)です。`);
+        return -1;
+    }
+
+    //改行だけでの送信を認めない
+    if(message == `\n`){
+        alert(`メッセージが改行のみです。`);
+        return -1;
+    }
+    if(message == `\r\n`){
+        alert(`メッセージが改行のみです。`);
+        return -1;
+    }
+    if(message == `\r`){
+        alert(`メッセージが改行のみです。`);
+        return -1;
+    }
+
+    //空白だけでの送信を認めない
+    if(message == ` `){
+        alert(`メッセージが半角空白のみです。`);
+        return -1;
+    }
+    if(message == `　`){
+        alert(`メッセージが全角空白のみです。`);
+        return -1;
+    }
+
     //投稿日時表示
     var Day = new Date();
 
