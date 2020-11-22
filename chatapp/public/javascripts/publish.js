68e47f1f9e33a31d1dd9ec090fb2eb41935a6535
mima-k <mimakouhei@gmail.com>
2020-11-23 05:19:50 +0900

    改行を正しく表示

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -5,20 +5,17 @@ function publish() {
     // ユーザ名を取得
     const userName = $('#userName').val() + 'さん:';
     // 入力されたメッセージを取得
-    const message = $('#message').val();
+    var message = $('#message').val();
+    message = message.replace(/\n/g, "<br>");
     //テキストを空にする
     $('#message').val('')
-    const data = ここから
+    const data = { userName: [userName], message: [message] };
     // 投稿内容を送信
-    socket.emit('publishMessage', message);
-    socket.emit('publishUserName', userName);
+    socket.emit('publishMessage', data);
     return false;
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
-socket.on('receiveMessage', function (message) {
-    $('#thread').prepend('<p>' + message + '</p>');
-});
-socket.on('receiveUserName', function (userName) {
-    $('#user-name').prepend('<p>' + userName + '</p>');
+socket.on('receiveMessage', function (data) {
+    $('tbody').prepend('<tr>' + '<th valign="top">' + '<p>' + data["userName"] + '</p>' + '</th>' + '<td>' + '<p>' + data["message"] + '</p>' + '</td>' + '</tr>');
 });
--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -2,11 +2,7 @@
 
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
-    socket.on('publishMessage', function (message) {
-        io.sockets.emit('receiveMessage', message);
-    });
-
-    socket.on('publishUserName', function (userName) {
-        io.sockets.emit('receiveUserName', userName);
+    socket.on('publishMessage', function (data) {
+        io.sockets.emit('receiveMessage', data);
     });
 };
