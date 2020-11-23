fed61158d21ae0116f480ae0aaa8c20ee1219a30
mikik0 <usapon_love1222@yahoo.co.jp>
2020-11-23 22:41:51 +0900

    いろいろ変えた

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -3,12 +3,21 @@
 // チャットルームに入室する
 function enter() {
     // 入力されたユーザ名を取得する
-    const userName = $('#userName').val();
+    const userName = $("#userName").val();
     // ユーザ名が未入力でないかチェックする
-    if(userName == ``){
-        alert(`ユーザ名が未入力です。`);
-        return -1;
+    if (userName !== '') {
+        $('form').submit();
+    } else {
+        alert('なまえをいれてね！')
     }
-
-$('form').submit();
 }
+
+$(function () {
+    $('input').keypress(function (e) {
+        var key = e.which;
+        if (key == 13) { // key13 == enter
+            enter()
+            return false;
+        }
+    });
+});
--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -8,42 +8,42 @@ function publish() {
     var message = $('#message').val();
     //投稿日時表示
     var Day = new Date();
-    Day = '(' + (Day.getMonth()+1) + '/' + Day.getDate() + '&nbsp' +  Day.getHours() + ':' + Day.getMinutes() + ')'　+ '&nbsp';
+    Day = '(' + (Day.getMonth() + 1) + '/' + Day.getDate() + '&nbsp' + Day.getHours() + ':' + Day.getMinutes() + ')' + '&nbsp';
     //テキストを空にする
     $('#message').val('');
 
     //空だけでの送信を認めない
-    if(message == ``){
+    if (message == '') {
         alert(`メッセージが空(から)です。`);
         return -1;
     }
     //改行だけでの送信を認めない
-    if(message == `\n`){
+    if (message == `\n`) {
         alert(`メッセージが改行のみです。`);
         return -1;
     }
-    if(message == `\r\n`){
+    if (message == `\r\n`) {
         alert(`メッセージが改行のみです。`);
         return -1;
     }
-    if(message == `\r`){
+    if (message == `\r`) {
         alert(`メッセージが改行のみです。`);
         return -1;
     }
 
     //空白だけでの送信を認めない
-    if(message == ` `){
+    if (message == ' ') {
         alert(`メッセージが半角空白のみです。`);
         return -1;
     }
-    if(message == `　`){
+    if (message == '　') {
         alert(`メッセージが全角空白のみです。`);
         return -1;
     }
 
     // 投稿内容を送信
     message = message.replace(/\n/g, "<br>");
-    const data = { userName: [userName], message: [message], day:[Day] };
+    const data = { userName: [userName], message: [message], day: [Day] };
     socket.emit('publishMessage', data);
     return false;
 }
@@ -51,5 +51,5 @@ function publish() {
 // サーバから受信した投稿メッセージを画面上に表示する
 socket.on('receiveMessage', function (data) {
     $('tbody').prepend('<tr>' + '<th valign="top">' + '<p>' + data["userName"] + '</p>' + '</th>'
-                       + '<td valign="top">' + '<p>' + data["day"] + '</p>' + '</td>' +'<td>' + '<p>' + data["message"] + '</p>' + '</td>' + '</tr>');
+        + '<td valign="top">' + '<p>' + data["day"] + '</p>' + '</td>' + '<td>' + '<p>' + data["message"] + '</p>' + '</td>' + '</tr>');
 });
